dd7b09076836a4bd10cfe0bc0d822f97752a0bcb
Denis <densid06@gmail.com>
2024-10-16 01:51:26 +0300

    Flappy Bird beta

--- FlappyBird/src/Controller.java
+++ FlappyBird/src/Controller.java
@@ -1,34 +1,142 @@
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.Node;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
+import java.util.Random;
 
 
 public class Controller {
    @FXML
-   ImageView birdPNG;
+   ImageView bird;
 
+   @FXML
+   ImageView tube;
+
+   @FXML
+   ImageView tube2;
+
+
+
+
+   @FXML
+   ImageView movingFon;
+   
+   @FXML
+   ImageView movingFon2;
   
-   public void jump() {
+   
+   @FXML
+   ImageView ground;
 
-        birdPNG.setLayoutY(birdPNG.getLayoutY() - 100);
-    
-}
- public void falling()
+      
+   @FXML
+   ImageView ground2;
+
+    Random randomNum = new Random();
+   int min = 306;
+   int max = 716;
+   
+
+
+
+
+
+
+   public void jump() 
+   {
+if (bird.getLayoutY()>100)
+{
+        bird.setLayoutY(bird.getLayoutY() - 100);
+      }   
+   }
+
+
+
+
+ public void birdFalling()
  {
   
-   birdPNG.setLayoutY(birdPNG.getLayoutY()+0.8);
+   bird.setLayoutY(bird.getLayoutY()+0.8);
 
-
-    
  }
+
+
+
+
+ public void moveTube()
+ {
+  tube.setLayoutX(tube.getLayoutX() - 0.5);  
+  tube2.setLayoutX(tube.getLayoutX());
+  tube2.setLayoutY(tube.getLayoutY()-1100);
+  if (tube.getLayoutX()<-162)
+  {
+    tube.setLayoutX(1272);
+    tube.setLayoutY(randomNum.nextInt(max - min + 1) + min);
+    
+  }
+
+
+
+
+ }
+
+
+ 
+
+ public void moveFon()
+ {
+  movingFon.setLayoutX(movingFon.getLayoutX() - 0.5);
+  if (movingFon.getLayoutX()==-1280)
+  {
+    movingFon.setLayoutX(1280);
+   
+  }
+ }
+
+
+
+ public void moveFon2()
+ {
+  movingFon2.setLayoutX(movingFon2.getLayoutX() - 0.5);
+  if (movingFon2.getLayoutX()==-1280)
+  {
+    movingFon2.setLayoutX(1280);
+  }
+ }
+
+
+
+
+ public void moveGround()
+ {
+  ground.setLayoutX(ground.getLayoutX() - 0.5);
+  if (ground.getLayoutX()==-1280)
+  {
+    ground.setLayoutX(1280);
+   
+  }
+ }
+
+
+
+
+
+ public void moveGround2()
+ {
+  ground2.setLayoutX(ground2.getLayoutX() - 0.5);
+  if (ground2.getLayoutX()==-1280)
+  {
+    ground2.setLayoutX(1280);
+  }
+ }
+
+public void gamestatus()
+{
+  if (bird.getBoundsInParent().intersects(ground.getBoundsInParent())||bird.getBoundsInParent().intersects(ground2.getBoundsInParent())||bird.getBoundsInParent().intersects(tube.getBoundsInParent())) {
+   // Platform.exit();
+    System.exit(1);
+  }
+
+}
+
 
 
 
--- FlappyBird/src/FlappyBird.java
+++ FlappyBird/src/FlappyBird.java
@@ -2,25 +2,18 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import java.util.Timer;
 import java.util.TimerTask;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
+  
 import javafx.event.EventHandler;
-import javafx.fxml.FXML;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 
 
 public class FlappyBird extends Application {
     private Controller controller;
-    
+        
     public static void main(String[] args)  
     {
         launch(args);
@@ -29,30 +22,48 @@ public class FlappyBird extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        
         FXMLLoader loader = new FXMLLoader(getClass().getResource("FlappyBird.fxml"));
         Parent root = loader.load();
         
         
         controller = loader.getController();
-        Scene FlappyBirdScene = new Scene(root, 800, 600);
-
+        Scene FlappyBirdScene = new Scene(root, 1280, 900);
 
         primaryStage.setTitle("Flappy Bird");
         primaryStage.setScene(FlappyBirdScene);
         primaryStage.show();
-    //    controller.falling();
+   
+        long birdFallSpeed =  3;
+        long tubeMoveSpeed =  2;
 
-        
- Timer gameTime = new Timer();
- 
-TimerTask task = new TimerTask() 
+Timer gameTime = new Timer();
+TimerTask birdFalling = new TimerTask() 
+
         {
         public void run()
         {
-            controller.falling();
+            controller.birdFalling();
         }
        };
-gameTime.schedule(task,0,3);  
+
+       TimerTask tubeMoving = new TimerTask() 
+
+       {
+       public void run()
+       {
+           controller.moveTube();
+         
+           controller.moveFon();
+           controller.moveFon2();
+           controller.moveGround();
+           controller.moveGround2();
+           controller.gamestatus(); 
+       }
+      };
+
+gameTime.schedule(birdFalling,0,birdFallSpeed);  
+gameTime.schedule(tubeMoving,0,tubeMoveSpeed);
 
 FlappyBirdScene.setOnKeyPressed(new EventHandler<KeyEvent>() 
 {
@@ -65,11 +76,6 @@ FlappyBirdScene.setOnKeyPressed(new EventHandler<KeyEvent>()
     }
 });
 
-//controller.jump(event);
- 
 
-        
-      
-        
     }
 }
